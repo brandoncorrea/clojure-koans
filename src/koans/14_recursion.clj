891d5d3a007b41adb63fa38ec02817609528a71f
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-03-07 13:32:36 -0500

    Solve koans 1 - 20

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -2,22 +2,60 @@
   (:require [koan-engine.core :refer :all]))
 
 (defn is-even? [n]
-  (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+  (
+    if (= n 0) true
+    (
+      if (= n 1) false 
+      (
+        is-even? (dec n)
+      )
+    )
+  )
+)
 
 (defn is-even-bigint? [n]
-  (loop [n   n
-         acc true]
-    (if (= n 0)
-      __
-      (recur (dec n) (not acc)))))
+  (
+    loop [
+      n n 
+      acc true]
+    (
+      if (= n 0)
+        false
+      (
+        recur 
+        (dec n) 
+        (not acc)
+      )
+    )
+  )
+)
 
 (defn recursive-reverse [coll]
-  __)
+  (
+    if (< (count coll) 2)
+      coll
+    (
+       cons 
+       (last coll) 
+       (recursive-reverse (butlast coll))
+    )
+  )
+)
 
 (defn factorial [n]
-  __)
+  (
+    loop [cur n
+      acc 1]
+    (
+      if (zero? cur) acc
+      (
+        recur
+        (dec cur)
+        (* acc cur)
+      )
+    )
+  )
+)
 
 (meditations
   "Recursion ends with a base case"
