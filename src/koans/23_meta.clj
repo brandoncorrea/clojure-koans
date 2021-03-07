8961fc28dac441320fbf0ea41f09a15c99ed3e2e
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2021-03-07 15:12:32 -0500

    Complete Koans

--- src/koans/23_meta.clj
+++ src/koans/23_meta.clj
@@ -6,46 +6,64 @@
     {:league "National League"}))
 
 (meditations
+  ; giants is like an object
   "Some objects can be tagged using the with-meta function"
-  (= __ (meta giants))
+  (= { :league "National League" } (meta giants))
 
+  ; This always returns, whether you put Giants, giants, or abc
+  ; Why?
   "Or more succinctly with a reader macro"
-  (= __ (meta '^{:division "West"} Giants))
+  (= {:division "West"} (meta '^{:division "West"} Giants))
 
+  ; This throws because 2 is not an object?
   "While others can't"
-  (= __ (try
+  (= "This doesn't implement the IObj interface"
+     (try
           (with-meta
             2
             {:prime true})
           (catch ClassCastException e
             "This doesn't implement the IObj interface")))
 
+  ; Repl gave me { :foo :bar } ... but why?
   "Notice when metadata carries over"
-  (= __ (meta (merge '^{:foo :bar} {:a 1 :b 2}
-                     {:b 3 :c 4})))
+  (= { :foo :bar }
+     (meta (merge '^{:foo :bar} {:a 1 :b 2} {:b 3 :c 4})))
 
+  ; When we switch the order, we get nil ... why?
   "And when it doesn't"
-  (= __ (meta (merge {:a 1 :b 2}
-                     '^{:foo :bar} {:b 3 :c 4})))
+  (= nil (meta (merge {:a 1 :b 2} '^{:foo :bar} {:b 3 :c 4})))
 
+  ; Getting char at index 0 - ok
   "Metadata can be used as a type hint to avoid reflection during runtime"
-  (= __ (#(.charAt ^String % 0) "Cast me"))
+  (= \C (#(.charAt ^String % 0) "Cast me"))
 
+  ; swap values - cool
   "You can directly update an object's metadata"
-  (= 8 (let [giants
-             (with-meta
-               'Giants
-               {:world-series-titles (atom 7)})]
-         (swap! (:world-series-titles (meta giants)) __)
-         @(:world-series-titles (meta giants))))
+  (= 8 (let
+        [
+          giants
+         ; Start with :world-series-titles = 7
+          (with-meta 'Giants {:world-series-titles (atom 7)})
+        ]
+        ; increment :world-series-titles
+        (swap! (:world-series-titles (meta giants)) inc)
+        ; Return :world-series-titles
+        @(:world-series-titles (meta giants))
+    )
+  )
 
+  ; before, giants only had league. now we associate park with giants
   "You can also create a new object from another object with metadata"
   (= {:league "National League" :park "AT&T Park"}
      (meta (vary-meta giants
-                      assoc __ __)))
+                      assoc :park "AT&T Park")))
 
+  ; vary-meta returns the with-meta quoted name
   "But it won't affect behavior like equality"
-  (= __ (vary-meta giants dissoc :league))
+  (= 'Giants (vary-meta giants dissoc :league))
 
+  ; So because the above problem returns (quote Giants),
+  ; pr-str casts that to a string
   "Or the object's printed representation"
-  (= __ (pr-str (vary-meta giants dissoc :league))))
+  (= "Giants" (pr-str (vary-meta giants dissoc :league))))
--- src/koans/24_macros.clj
+++ src/koans/24_macros.clj
@@ -9,36 +9,48 @@
 
 (defmacro infix-concise [form]
   `(~(second form) ; Note the syntax-quote (`) and unquote (~) characters!
-    __
-    __))
+    ~(first form)
+    ~(nth form 2)))
 
 (defmacro recursive-infix [form]
-  (cond (not (seq? form))
-        __
-        (= 1 (count form))
-        `(recursive-infix ~(first form))
-        :else
-        (let [operator (second form)
+  (cond
+    ; if this is not a sequence, then it is a value
+    (not (seq? form))
+      form
+    ; If this sequence has one item, then it is a (...)
+    (= 1 (count form))
+      `(recursive-infix ~(first form))
+    ; We have stuff to play with...
+    :else
+      (let [operator (second form)
               first-arg (first form)
-              others __]
-          `(~operator
-            (recursive-infix ~first-arg)
-            (recursive-infix ~others)))))
+              others (drop 2 form)]
+        `(~operator
+         (recursive-infix ~first-arg)
+         (recursive-infix ~others))
+      )
+  )
+)
 
 (meditations
   "Macros are like functions created at compile time"
-  (= __ (hello "Macros!"))
+  (= "Hello, Macros!" (hello "Macros!"))
 
   "I can haz infix?"
-  (= __ (infix (9 + 1)))
+  (= 10 (infix (9 + 1)))
 
+  ; '(infix (9 + 1)) => (infix (9 + 1)
+  ; Note: no beginning quote
+  ; macroexpand removes the macro name and top-level parentheses
   "Remember, these are nothing but code transformations"
-  (= __ (macroexpand '(infix (9 + 1))))
+  (= '(+ 9 1) (macroexpand '(infix (9 + 1))))
 
   "You can do better than that - hand crafting FTW!"
   (= '(* 10 2) (macroexpand '(infix-concise (10 * 2))))
 
   "Things don't always work as you would like them to... "
+  ; ... Or do they?
+  ; This passed when I solved the problem above but I have no idea why
   (= '(+ 10 (2 * 3)) (macroexpand '(infix-concise (10 + (2 * 3)))))
 
   "Really, you don't understand recursion until you understand recursion"
